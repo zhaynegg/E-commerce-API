bd3efe875ff80532df8497e645abd4da16d900d0
zhaynegg <zhasynea@gmail.com>
2025-05-24 10:40:16 +0500

    Added libraties

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
 import psycopg2
-
+from script2 import mypassword
 app = Flask(__name__)
+
+# create db with images
+# create a platform to choose the products
+# have an ability to see info about the product
+# have an ability to add the product into the basket
+
+# Current problems: payment how it will be done
+# Add login/registration form. I do not really know what to use
 
 if __name__ == "__main__":
     app.run(debug=True)
